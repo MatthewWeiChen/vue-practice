e5f907934730e00702b5690643b579550cef4865
MatthewWeiChen <mattchen1021@gmail.com>
2020-12-31 13:13:39 -0800

    added event dynamic argument

--- core/basics-events-starting-code/app.js
+++ core/basics-events-starting-code/app.js
@@ -5,11 +5,11 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    increaseCounter() {
-      this.counter++;
+    increaseCounter(num) {
+      this.counter = this.counter + num;
     },
-    decreaseCounter() {
-      this.counter--;
+    decreaseCounter(num) {
+      this.counter = this.counter - num;
     },
   },
 });
