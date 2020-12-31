4eff0fed8e6026c26350acb22c240d36cf8c3534
MatthewWeiChen <mattchen1021@gmail.com>
2020-12-31 11:12:06 -0800

    added method in html file

--- core/app.js
+++ core/app.js
@@ -2,7 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       courseGoal: "Finish the course and learn Vue!",
+      vueLink: "https://vuejs.org/",
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Master Vue!";
+      }
+    },
   },
 });
 
