5fdb677846e1627cbd45417c42e592546a27485a
MatthewWeiChen <mattchen1021@gmail.com>
2020-12-31 02:25:10 -0800

    rebuilt starter project in vue

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemElement = document.createElement("li");
-  listItemElement.textContent = enteredValue;
-  listEl.appendChild(listItemElement);
-  inputEl.value = "";
-}
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemElement = document.createElement("li");
+//   listItemElement.textContent = enteredValue;
+//   listEl.appendChild(listItemElement);
+//   inputEl.value = "";
+// }
 
-buttonEl.addEventListener("click", addGoal);
+// buttonEl.addEventListener("click", addGoal);
+
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+    },
+  },
+}).mount("#app");
