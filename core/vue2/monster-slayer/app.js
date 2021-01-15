eb8266eb3fa97811a4608b30e5b636e2ff6b9b1a
MatthewWeiChen <mattchen1021@gmail.com>
2021-01-14 16:52:16 -0800

    completed mvp

--- core/vue2/monster-slayer/app.js
+++ core/vue2/monster-slayer/app.js
@@ -1,3 +1,7 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 new Vue({
   el: "#app",
   data: {
@@ -10,6 +14,35 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack() {
+      this.monsterHealth -= getRandomValue(5, 12);
+      this.monsterAttack();
+    },
+    monsterAttack() {
+      this.playerHealth -= getRandomValue(8, 15);
+    },
+    heal() {
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.monsterAttack();
+    },
+    specialAttack() {
+      this.monsterHealth -= getRandomValue(8, 20);
+      this.monsterAttack();
+    },
+    giveUp() {},
+  },
+  computed: {
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
     },
   },
 });
