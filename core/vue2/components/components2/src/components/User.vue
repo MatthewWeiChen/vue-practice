9054d282b1529e6a273508c0e2962da4afd98b67
MatthewWeiChen <mattchen1021@gmail.com>
2021-01-23 23:56:08 -0800

    started

---
+++ core/vue2/components/components2/src/components/User.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change My Name</button>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <UserDetail :myName="name"></UserDetail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <UserEdit></UserEdit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//Parent Component
+import UserDetail from './UserDetail.vue';
+import UserEdit from './UserEdit.vue';
+
+export default {
+  components: {
+    UserDetail,
+    UserEdit,
+  },
+  data() {
+    return {
+      name: 'Matt',
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = 'Anna';
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightblue;
+}
+</style>
---
+++ core/vue2/components/components2/src/components/UserDetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ switchName() }}</p>
+  </div>
+</template>
+
+<script>
+//Child Component
+export default {
+  props: {
+    myName: String,
+    required: true,
+  },
+  methods: {
+    switchName() {
+      return this.myName.split('').reverse().join('');
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightcoral;
+}
+</style>
